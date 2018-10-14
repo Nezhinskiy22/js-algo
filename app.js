a57bd5c2f4cb2521f8268d71e2cf4bbb833f331e
Nezhinskiy22 <nezhinskiy22@gmail.com>
2018-10-14 23:30:27 +0300

    Last

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
     function writeAlgoDemo1() {
         var side_a = Math.random() * 10 + 5;
         var side_b = Math.random() * 10 + 5;
-        document.write("Side a" + side_a + "Side d" + side_b);
+        document.write("Side a" + side_a + "Side b" + side_b);
         document.write("Объем куба" + side_a * side_a * side_a);
         document.write("Площадь квадрата" + side_a * side_a);
         document.write("Площадь прямоугольника" + side_a * side_b);
