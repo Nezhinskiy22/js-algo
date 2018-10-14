dbcd547378983cc5ba9a237749cc46c5f2a49ab8
Nezhinskiy22 <nezhinskiy22@gmail.com>
2018-10-14 22:01:29 +0300

    Changes for next implementation of algorithms

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ function writeAlgoDemo() {
     var side_b = Math.random() * 10 + 5;
     document.write("Side a" + side_a + "Side d" + side_b);
     document.write("Объем куба" + side_a * side_a * side_a);
-    document.write("Плащадь квадрата" + side_a * side_a);
+    document.write("Площадь квадрата" + side_a * side_a);
     document.write("Площадь прямоугольника" + side_a * side_b);
 }
